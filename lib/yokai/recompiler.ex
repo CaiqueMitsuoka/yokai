82255dd45bc80b890c4067e5463fd5e291553981
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2025-07-18 01:58:27 -0300

    feat: exs files recompile without warnings

--- lib/yokai/recompiler.ex
+++ lib/yokai/recompiler.ex
@@ -6,6 +6,8 @@ defmodule Yokai.Recompiler do
   use GenServer
   require Logger
 
+  alias Yokai.ExsRecompiler
+
   def start_link do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
@@ -29,75 +31,10 @@ defmodule Yokai.Recompiler do
 
   @impl true
   def handle_call({:tests, pattern}, _caller, state) do
-    {:reply, compile_tests(pattern), state}
+    {:reply, ExsRecompiler.from_pattern(pattern), state}
   end
 
   defp compile_code do
-    try do
-      IEx.Helpers.recompile()
-    rescue
-      e in FunctionClauseError ->
-        Logger.error("Error during recompilation: #{Exception.message(e)}")
-        Logger.info("Falling back to Mix.Task.rerun(\"compile\")")
-        Mix.Task.rerun("compile")
-    end
-  end
-
-  defp compile_tests(test_files_pattern) do
-    with :ok <- purge_test_modules(),
-         test_files <- Path.wildcard(test_files_pattern),
-         :ok <- compile_test_files(test_files),
-         :ok <- reload_test_helper(),
-         :ok <- load_test_files(test_files) do
-      :ok
-    else
-      _ -> :error
-    end
-  end
-
-  def purge_test_modules do
-    for {mod, _file} <- Code.required_files(), do: :code.purge(mod)
-    for {mod, _file} <- Code.required_files(), do: :code.delete(mod)
-    :ok
-  end
-
-  def compile_test_files(test_files) do
-    try do
-      Enum.each(test_files, &Code.compile_file/1)
-      :ok
-    rescue
-      e in CompileError ->
-        Logger.error("Error during test compilation: #{Exception.message(e)}")
-        :error
-    end
-  end
-
-  def load_test_files(test_files) do
-    try do
-      Enum.each(test_files, fn file ->
-        Logger.info("Loading test file: #{file}")
-        Code.require_file(file)
-      end)
-
-      :ok
-    rescue
-      e in CompileError ->
-        Logger.error("Error during test loading: #{Exception.message(e)}")
-        :error
-    end
-  end
-
-  defp reload_test_helper do
-    Logger.info("Reloading test_helper.exs")
-    test_helper_path = "test/test_helper.exs"
-
-    if File.exists?(test_helper_path) do
-      Code.unrequire_files([test_helper_path])
-      Code.require_file(test_helper_path)
-      :ok
-    else
-      Logger.error("test_helper.exs not found")
-      :error
-    end
+    IEx.Helpers.recompile()
   end
 end
---
+++ lib/yokai/test_recompiler.ex
@@ -0,0 +1,91 @@
+defmodule Yokai.ExsRecompiler do
+  require Logger
+
+  def from_pattern(test_files_pattern) do
+    with test_files <- Path.wildcard(test_files_pattern),
+         :ok <- purge_test_modules(test_files),
+         :ok <- compile_test_files(test_files),
+         :ok <- reload_test_helper(),
+         :ok <- load_test_files(test_files) do
+      :ok
+    else
+      _ -> :error
+    end
+  end
+
+  def purge_test_modules(test_paths) do
+    Enum.each(test_paths, &reload_test_file/1)
+
+    :ok
+  end
+
+  def reload_test_file(file_path) do
+    modules_from_file = find_modules_from_file(file_path)
+
+    Enum.each(modules_from_file, fn module ->
+      :code.purge(module)
+      :code.delete(module)
+    end)
+  end
+
+  defp find_modules_from_file(file_path) do
+    absolute_path = Path.expand(file_path)
+
+    :code.all_loaded()
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.filter(fn module ->
+      case get_source_file(module) do
+        ^absolute_path -> true
+        _ -> false
+      end
+    end)
+  end
+
+  defp get_source_file(module) do
+    try do
+      module.__info__(:compile)[:source] |> to_string() |> Path.expand()
+    rescue
+      _ -> nil
+    end
+  end
+
+  def compile_test_files(test_files) do
+    try do
+      Enum.each(test_files, &Code.compile_file/1)
+      :ok
+    rescue
+      e in CompileError ->
+        Logger.error("Error during test compilation: #{Exception.message(e)}")
+        :error
+    end
+  end
+
+  def load_test_files(test_files) do
+    try do
+      Enum.each(test_files, fn file ->
+        Logger.info("Loading test file: #{file}")
+        Code.require_file(file)
+      end)
+
+      :ok
+    rescue
+      e in CompileError ->
+        Logger.error("Error during test loading: #{Exception.message(e)}")
+        :error
+    end
+  end
+
+  defp reload_test_helper do
+    Logger.info("Reloading test_helper.exs")
+    test_helper_path = "test/test_helper.exs"
+
+    if File.exists?(test_helper_path) do
+      Code.unrequire_files([test_helper_path])
+      Code.require_file(test_helper_path)
+      :ok
+    else
+      Logger.error("test_helper.exs not found")
+      :error
+    end
+  end
+end
